ec84d44791ab5b8aa6b94077474ff843c2fca394
BumBimz <l3uml3imz@gmail.com>
2015-11-03 09:02:36 +0700

    refactor

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class QueueTest {
+    Queue queue = new Queue();
+
     @Test
     public void isEmptyQueue() {
-        Queue queue = new Queue();
         assertTrue("Queue is not Empty", queue.isEmpty());
     }
 
     @Test
     public void isNotEmptyQueue() {
-        Queue queue = new Queue();
         queue.enQueue("A");
         assertFalse("Queue is Empty", queue.isEmpty());
     }
